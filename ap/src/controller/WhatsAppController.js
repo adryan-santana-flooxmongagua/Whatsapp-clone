609207272449be036de2a465d134f0d5592b940f
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-02-11 09:19:48 -0300

    feat: create main app controller

--- ap/src/controller/WhatsAppController.js
+++ ap/src/controller/WhatsAppController.js
@@ -1,8 +1,24 @@
 class WhatsAppController {
     constructor() {
 
-
         console.log('ok');
+
+        this.loadElements();
+
+    }
+
+    loadElements(){
+
+        this.el = {};
+
+        document.querySelectorAll('[id]').forEach(element=>{
+
+
+            this.el[FormData.getCamelCase(element.id)] = element;
+
+        });
 
     }
 }
+
+
