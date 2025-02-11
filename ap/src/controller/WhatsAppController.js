50093a0572416a17f97a643d57fd450bb6a91183
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-02-11 11:11:03 -0300

    feat: add events to toggle 'Edit Profile' and 'Add Contact' panels

--- ap/src/controller/WhatsAppController.js
+++ ap/src/controller/WhatsAppController.js
@@ -5,6 +5,7 @@ class WhatsAppController {
 
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
 
     }
 
@@ -95,6 +96,52 @@ class WhatsAppController {
         }
 
     }
+
+    initEvents(){
+
+        this.el.myPhoto.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(()=>{
+                this.el.panelEditProfile.addClass('open');
+            }, 300);
+            
+
+        });
+
+        this.el.btnNewContact.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{
+                this.el.panelAddContact.addClass('open');
+            }, 300);
+            
+
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e=>{
+
+            this.el.panelEditProfile.removeClass('open');
+
+        });
+       
+        this.el.btnClosePanelAddContact.on('click', e=>{
+
+            this.el.panelAddContact.removeClass('open');
+
+        });
+
+    }
+
+    closeAllLeftPanel(){
+
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
+
+    }
+
 }
 
 
