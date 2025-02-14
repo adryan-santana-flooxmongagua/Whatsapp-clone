12c00c036862d42bce36a25c3a07831c6ee034ab
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-02-14 11:41:58 -0300

    feat: capture photo from camera using canvas

--- app/src/controller/CameraController.js
+++ app/src/controller/CameraController.js
@@ -1,31 +1,32 @@
 export class CameraController {
     constructor(videoEl) {
-        
         this._videoEl = videoEl;
 
         navigator.mediaDevices.getUserMedia({
             video: true
-        }).then(stream=>{
-
+        }).then(stream => {
             this._stream = stream;
-            this._videoEl.src = URL.createObjectURL(stream);
+            this._videoEl.srcObject = stream; // Correção aqui
             this._videoEl.play();
-
-        }).catch(err=>{
-
-            console.error(err);
-
+        }).catch(err => {
+            console.error("Erro ao acessar a câmera:", err);
         });
-
     }
 
-    stop(){
+    stop() {
+        if (this._stream) {
+            this._stream.getTracks().forEach(track => track.stop());
+        }
+    }
 
-        this._stream.getTracks().forEach(track=>{
+    takePicture(mimeType = 'image/png') {
+        let canvas = document.createElement('canvas');
+        canvas.width = this._videoEl.videoWidth;
+        canvas.height = this._videoEl.videoHeight;
 
-            track.stop();
-           })
+        let context = canvas.getContext('2d');
+        context.drawImage(this._videoEl, 0, 0, canvas.width, canvas.height);
 
-
+        return canvas.toDataURL(mimeType);
     }
 }
