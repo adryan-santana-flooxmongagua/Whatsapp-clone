6aec2b16b7f431090a2477c973d7cce94a692b54
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-02-18 09:42:00 -0300

    feat: enable microphone with MediaDevices.getUserMedia API

---
+++ app/src/controller/MicrophoneController.js
@@ -0,0 +1,30 @@
+export class MicrophoneController{
+    constructor(){
+
+        navigator.mediaDevices.getUserMedia({
+            audio: true
+        }).then(stream => {
+            this._stream = stream;
+           
+            let audio = new Audio();
+
+            audio.src = URL.createObjectURL(stream);
+
+            audio.play();
+
+            this.trigger('play', audio);
+
+        }).catch(err => {
+            console.error(err);
+        });
+        
+
+    }
+
+    stop() {
+        if (this._stream) {
+            this._stream.getTracks().forEach(track => track.stop());
+        }
+    }
+
+}
