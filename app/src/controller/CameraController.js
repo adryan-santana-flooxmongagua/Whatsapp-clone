39e46fad9b6d1ac1603ea0320a88a342154c7dda
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-02-19 08:35:01 -0300

    fix: resolve webcam functionality issues

--- app/src/controller/CameraController.js
+++ app/src/controller/CameraController.js
@@ -1,48 +1,33 @@
 export class CameraController {
 
-    constructor(videoEl){
-
+    constructor(videoEl) {
         this._videoEl = videoEl;
 
-        navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-
-            this._stream = stream;
-
-            this._videoEl.src = URL.createObjectURL(stream);
-
-            this._videoEl.play();
-
-        }).catch(err => {
-
-            console.error(err);
-
-        });
-
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(stream => {
+                this._stream = stream;
+                this._videoEl.srcObject = stream; // Correção aqui
+                this._videoEl.play();
+            })
+            .catch(err => {
+                console.error("Erro ao acessar a câmera:", err);
+            });
     }
 
-    takePicture(mimeType = 'image/png'){
-
+    takePicture(mimeType = 'image/png') {
         let canvas = document.createElement('canvas');
-
-        canvas.setAttribute('height', this._videoEl.videoHeight);
-        canvas.setAttribute('width', this._videoEl.videoWidth);
+        canvas.width = this._videoEl.videoWidth;
+        canvas.height = this._videoEl.videoHeight;
 
         let context = canvas.getContext('2d');
-
         context.drawImage(this._videoEl, 0, 0, canvas.width, canvas.height);
 
         return canvas.toDataURL(mimeType);
-
     }
 
-    stop(){
-
-        this._stream.getTracks().forEach(track => {
-
-            track.stop();
-
-        });
-
+    stop() {
+        if (this._stream) {
+            this._stream.getTracks().forEach(track => track.stop());
+        }
     }
-
 }
