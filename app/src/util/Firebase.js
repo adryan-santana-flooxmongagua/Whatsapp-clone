5eba133288f1370486e5214b1c9d2b7b92ebead8
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-02-20 11:55:31 -0300

    feat: save authenticated user in database

--- app/src/util/Firebase.js
+++ app/src/util/Firebase.js
@@ -21,7 +21,7 @@ export class Firebase{
 
     init(){
 
-        if(!this._initialized){
+        if(!window._initializedFirebase){
 
        firebase.initializeApp(this._config);
 
@@ -29,7 +29,7 @@ export class Firebase{
         timestampsInSnapshots: true
        });
 
-       this._initialized = true;
+       window._initializedFirebase = true;
 
         }
 
